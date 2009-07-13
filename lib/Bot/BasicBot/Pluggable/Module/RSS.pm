510fdfa5221080b8496d2fd6dfcb5d20c44c2248
Mario Domgoergen <dom@math.uni-bonn.de>
2009-07-13 22:40:22 +0200

    now using POE::Component::RSSAggregator

--- lib/Bot/BasicBot/Pluggable/Module/RSS.pm
+++ lib/Bot/BasicBot/Pluggable/Module/RSS.pm
@@ -2,38 +2,65 @@ package Bot::BasicBot::Pluggable::Module::RSS;
 
 use warnings;
 use strict;
-use XML::Feed;
-use LWP::UserAgent;
-use HTTP::Status qw(:is);
+use POE;
+use POE::Component::RSSAggregator;
+use Digest::MD5 qw(md5_hex);
 
 use base qw(Bot::BasicBot::Pluggable::Module);
 
 sub init {
-	my $self = shift;
-	my $ua = LWP::UserAgent->new();
-	$self->config({
-		user_http_timeout => 10,
-	});
-	
-	$ua->timeout($self->get('user_http_timeout'));
-	$ua->env_proxy;
-	
-	$self->{ua} = $ua;
-	$self->{feeds} = $self->get('feeds');
+    my $self = shift;
+    $self->config( { feeds => {}, delay => 10, init_headlines_seen => 1 } );
+    $self->{feeds} = $self->get('feeds');
+
+    POE::Session->create(
+        inline_states => {
+            _start      => \&init_session,
+            handle_feed => \&handle_feed,
+        },
+        args => [$self],
+    );
+
 }
 
-sub start {
-	my ($self) = @_;
-	for my $channel (keys %{$self->get('feeds')}) {
-		for my $uri (keys %{$self->{feeds}->{$channel}}) {
-			$self->add_feed($channel,$uri);
-		}
+sub init_session {
+    my ( $kernel, $heap, $session, $module ) = @_[ KERNEL, HEAP, SESSION, ARG0 ];
+    $heap->{rssagg} = POE::Component::RSSAggregator->new(
+        alias    => 'rssagg',
+        debug    => 1,
+        callback => $session->postback("handle_feed"),
+        tmpdir   => '/tmp',                              # optional caching
+    );
+    $heap->{module} = $module;
+	foreach my $uri ( keys %{ $module->{feeds}} ) {
+    		$kernel->post( 'rssagg', 'add_feed', $module->new_feed($uri));
 	}
 }
 
-sub said {
-	my ($self,$message) = @_;
+sub new_feed {
+    my ( $self, $uri ) = @_;
+    my $name = md5_hex($uri);	
+    return {
+        url                 => $uri,
+        name                => $name,
+        delay               => $self->get('delay'),
+        init_headlines_seen => $self->get('init_headlines_seen'),
+    };
+}
 
+sub handle_feed {
+    my ( $kernel, $feed, $heap ) = ( $_[KERNEL], $_[ARG1]->[0], $_[HEAP] );
+    my $module   = $heap->{module};
+    my $uri      = $feed->url();
+    my $feeds = $module->get('feeds');
+    my @channels = keys %{ $feeds->{$uri} };
+    for my $headline ( $feed->late_breaking_news ) {
+        $module->tell( $_, $headline->headline ) for @channels;
+    }
+}
+
+sub told {
+	my ($self,$message) = @_;
 	# Only act if we are addressed
 	if ($message->{address}) {
 		my $body    = $message->{body};
@@ -46,33 +73,43 @@ sub said {
 		my @cmds = split(' ',$body);
 		if ($cmds[0] eq 'rss') {
 			my %actions = (
-				add    => sub { return $self->add_feed( $channel, $_[0] ) },
+				add    => sub { return $self->add_feed( $channel, @_ ) },
 				list   => sub { return $self->list_feeds($channel); },
-				remove => sub { return $self->remove_feed( $channel, $_[0] ) },
+				remove => sub { return $self->remove_feed( $channel, @_ ) },
 			);
-			return $actions{$cmds[1]}->(@cmds[2,-1]);
+			if (!defined($actions{$cmds[1]})) {
+				return $self->help();
+			}
+			my $reply = $actions{$cmds[1]}->(@cmds[2,-1]) ;
+			return $reply;
 		}
 	}
 }
 
 sub add_feed {
 	my ($self,$channel,$uri) = @_;
-	if ($uri) {
-		$self->{feeds}->{$channel}->{$uri} = 1;
-		$self->set('feeds', keys %{$self->{feeds}});
-		my $feed = XML::Feed->parse(URI->new($uri)) or return XML::Feed->errstr;
-		for my $entry ($feed->entries()) {
-			$self->{seen}->{$uri}->{$entry->title} = 1
-		}
-		return "Okay, added $uri."
+	if ($uri and ! $self->{feeds}->{$uri}->{$channel}) {
+		$self->{feeds}->{$uri}->{$channel} = 1;
+		$self->set('feeds', $self->{feeds});
+    		POE::Kernel->post( 'rssagg', 'add_feed', $self->new_feed($uri));
+		return "Ok.";
 	}
+	return "Did you forget the uri or was this channel already added?";
 }
 
 sub remove_feed {
 	my ($self,$channel,$uri) = @_;
-	if ( $self->{feeds}->{$channel}->{$uri} ) {
-		delete $self->{feeds}->{$channel}->{$uri};
+	if ( $self->{feeds}->{$uri}->{$channel} ) {
+		my $name = $self->{feeds}->{$uri}->{$channel};
+		delete $self->{feeds}->{$uri}->{$channel};
 		$self->set('feeds', keys %{$self->{feeds}});
+		## We remove the feed from poco if it's the last
+		if (!keys %{$self->{feeds}->{$uri}}) {
+			delete $self->{feeds}->{$uri};
+			my $name = md5_hex($uri);
+    			POE::Kernel->post( 'rssagg', 'remove_feed', $name );
+		}
+		return "Ok.";
 	} else {
 		return "Mhh, i don't even know about that url";
 	}
@@ -81,40 +118,21 @@ sub remove_feed {
 sub list_feeds {
 	my ($self,$channel) = @_;
 	my $reply;
-	for my $uri (keys %{$self->{feeds}->{$channel}}) {
-		$reply .= $uri . "\n";
+	for my $uri (keys %{$self->{feeds}}) {
+		if ($self->{feeds}->{$uri}->{$channel} ) {
+			$reply .= "$uri\n" ;
+		}
 	}
-	return $reply;
-}
-
-sub reply {
-	my ($self) = @_;
-	for my $channel (keys %{$self->{feeds}}) {
-		my $reply;
-		for my $uri (keys %{$self->{feeds}->{$channel}}) {
-			$self->{ua}->get($uri);
-			if (is_success($response->code)){
-				my $feed = XML::Feed->parse($response->content()) or warn XML::Feed->errstr;
-				for my $entry ($feed->entries()) {
-					if (! $self->{seen}->{$uri}->{$entry->title}++ ) {
-						$reply .= $entry->title . "\n";
-					}
-				}
-			}
-		}
-		if ($reply) {
-			$self->tell($channel, $reply);
-		}
+	if ($reply) {
+		return $reply;
+	} else {
+		return 'Nobody added rss feeds to me yet.';
 	}
 }
 
-sub tick {
-	$feeds = map { @{$self->{feeds}->{$_}} } keys %{$self->{feeds}}
-	if ($self->{last_run} > time)
+sub help {
+	return "rss [add uri|remove|list]";
 }
-
-		
-				
 
 =head1 NAME
 
@@ -135,10 +153,10 @@ Quick summary of what the module does.
 
 Perhaps a little code snippet.
 
-    use Bot::BasicBot::Pluggable::Module::RSS;
-
-    my $foo = Bot::BasicBot::Pluggable::Module::RSS->new();
-    ...
+    !load RSS
+    rss add http://search.cpan.org/uploads.rdf
+    rss list
+    rss remove http://search.cpan.org/uploads.rdf
 
 =head1 EXPORT
 
